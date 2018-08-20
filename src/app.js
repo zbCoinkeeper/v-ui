a38a5c37f295dea15cdcf03128ef97b2c9c52cb4
zb <2479107942@qq.com>
2018-08-20 17:32:02 +0800

    添加了spy监听测试

--- src/app.js
+++ src/app.js
@@ -15,4 +15,103 @@ new Vue({
     }
 })
 
+import chai from 'chai'
+import spies from 'chai-spies'
+const expect = chai.expect
+chai.use(spies)
 
+
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    }
+    )
+    vm.$mount()
+    console.log(vm.$el)
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    }
+    )
+    vm.$mount()
+    console.log(vm.$el)
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    vm.$el.remove()
+    vm.$destroy()
+
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: "right"
+        }
+    }
+    )
+    vm.$mount(div)
+    console.log(vm.$el)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    }
+    )
+    vm.$mount(div)
+    console.log(vm.$el)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    }
+    )
+    vm.$mount()
+    let spy=chai.spy(()=>{})
+    vm.$on('click',spy)
+    let button = vm.$el;
+    button.click()
+    expect(spy).to.have.been.called()
+    vm.$el.remove()
+    vm.$destroy()
+}
--- src/button.vue
+++ src/button.vue
@@ -10,81 +10,82 @@
 </template>
 
 <script>
-        export default {
-            //props:['icon','iconPosition']
-            props:{
-                icon:{},
-                iconPosition:{
-                    type:String,
-                    default:"left",
-                    validator(value){
-
-                        //if(value!=="left"&&value!=="right"){
-                        //    return false
-                        //}else{
-                        //    return true
-                        //}简化写法
-                        return value==="left"||value==="right"
-                    }
-                },
-                loading:{
-                    type:Boolean,
-                    default:false,
-                }
-            }
-        };
+export default {
+  //props:['icon','iconPosition']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        //if(value!=="left"&&value!=="right"){
+        //    return false
+        //}else{
+        //    return true
+        //}简化写法
+        return value === "left" || value === "right";
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
 </script>
 
     <style lang="scss">
-        @keyframes spin{
-            0%{transform: rotate(0deg);}
-            100%{transform: rotate(360deg)}
-        }
-
-
-        .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        display:inline-flex;
-        justify-content:center;
-        align-items:center;
-        vertical-align: top;   /*inline元素就是如此*/
-        >.content{
-            order:2;
-        }
-        >.icon{
-            order:1;
-            margin-left:0;
-            margin-right:.3em;
-        }
-        &.icon-right{
-            >.content{
-             order:1;
-            }
-            >.icon{
-            order:2;
-            margin-left:.3em;
-            margin-right:0;
-            }
-        }
-
-
-&:hover {
-            border-color: var(--border-color-hover);
-      }
-      
-&:active {
-            background-color:var(--button-active-bg);
-          }
-&:focus{
-    outline:none;
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
-    .loading{
-        animation: spin 2s infinite linear;
+
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: top; /*inline元素就是如此*/
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-left: 0;
+    margin-right: 0.3em;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
     }
-      }
+    > .icon {
+      order: 2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
+  }
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  .loading {
+    animation: spin 2s infinite linear;
+  }
+}
 </style>
