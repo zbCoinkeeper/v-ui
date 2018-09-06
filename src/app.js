d6eb7504e0f7a36885cc1dd95b4687eeba247b3e
zb <2479107942@qq.com>
2018-09-06 11:28:45 +0800

    修复icon问题

--- src/app.js
+++ src/app.js
@@ -7,146 +7,26 @@ import Input from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input',Input)
-
+Vue.component('g-input', Input)
 
 new Vue({
-    el: '#app',
-    data: {
-        loading1: false
+  el: '#app',
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false,
+    message: 'hi'
+  },
+  created(){
+  },
+  methods: {
+    inputChange (e) {
+      console.log(e)
     }
+  }
 })
 
 
 
 
 
-
-import chai from 'chai'
-import spies from 'chai-spies'
-const expect = chai.expect
-chai.use(spies)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//单元测试
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting'
-        }
-    }
-    )
-    vm.$mount()
-    console.log(vm.$el)
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-setting')
-    vm.$el.remove()
-    vm.$destroy()
-}   
-
-{
-
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-            loading: true
-        }
-    }
-    )
-    vm.$mount()
-    console.log(vm.$el)
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-loading')
-    vm.$el.remove()
-    vm.$destroy()
-
-}
-
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-            iconPosition: "right"
-        }
-    }
-    )
-    vm.$mount(div)
-    console.log(vm.$el)
-    let svg = vm.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-        }
-    }
-    )
-    vm.$mount(div)
-    console.log(vm.$el)
-    let svg = vm.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-        }
-    }
-    )
-    vm.$mount()
-    let spy=chai.spy(()=>{})
-    vm.$on('click',spy)
-    let button = vm.$el;
-    button.click()
-    expect(spy).to.have.been.called()
-    vm.$el.remove()
-    vm.$destroy()
-}
