714f30a8c367750e201d35decb2747b6a7ce5aa8
zb <2479107942@qq.com>
2018-09-09 19:42:31 +0800

    添加了plugin.js

---
+++ src/plugin.js
@@ -0,0 +1,22 @@
+import Toast from './toast'
+
+
+let currentToast
+
+export default {
+  install (Vue, options) {
+    Vue.prototype.$toast = function (message, toastOptions) {
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({
+        Vue,
+        message,
+        propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
+        }
+      })
+    }
+  }
+}
