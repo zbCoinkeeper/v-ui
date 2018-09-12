b6273ee430deca4e2d7cdb232257eb55082121e5
zb <2479107942@qq.com>
2018-09-12 10:42:44 +0800

    提交了content.vue

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
+
+
+
 
 
 
@@ -14,8 +24,18 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
+Vue.use(plugin) 
 
 
+import createElement from 'vue'
+
+const h = createElement
 
 new Vue({
   el: '#app',
@@ -28,12 +48,30 @@ new Vue({
   created(){
   },
   methods: {
-    inputChange (e) {
-      console.log(e)
+    showToast1(){
+      this.showToast('top')
+    },
+    showToast2(){
+      this.showToast('middle')
+    },
+    showToast3(){
+      this.showToast('bottom')
+    },
+    showToast(position){
+      this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+        position,
+        enableHtml: false,
+        closeButton: {
+          text: '已充值',
+          callback () {
+            console.log('他说已经充值智商了')
+          }
+        },
+        autoClose: 3,
+      })
     }
   }
 })
-
 
 
 
