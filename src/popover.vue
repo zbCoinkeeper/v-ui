b1fa8f19f1bc14beda1f9c6fe4ed84548fbee9fe
zb <2479107942@qq.com>
2019-04-22 20:32:04 +0800

    初步完成popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="gulu-popover">
-        <div class="gulu-popover-content-wrapper" v-if="isPop">
+    <div class="gulu-popover" v-click-outside="onClosePopover">
+        <div ref="content-wrapper" class="gulu-popover-content-wrapper" v-if="isPop">
             <slot name="content"></slot>
         </div>
-        <div class="gulu-popover-trigger" @click="ontrigger">
+        <div ref="trigger" class="gulu-popover-trigger" @click="ontrigger">
             <slot></slot>
         </div>
     </div>
 </template>
 
 <script>
+    import ClickOutside from "./click-outside"
     export default {
         name: "popover",
         data(){
@@ -17,9 +18,24 @@
                 isPop:false
             }
         },
+        directives:{
+          ClickOutside
+        },
         methods:{
             ontrigger(){
                 this.isPop=!this.isPop
+                if(!this.isPop){
+                    console.log("关闭")
+                }else{
+                    this.$nextTick(()=>{
+                        console.log(this.$refs.contentWrapper)
+                        this.$refs.contentWrapper.style.left=window.screenX+this.$refs.trigger.left;
+                        this.$refs.contentWrapper.style.top=window.screenY+this.$refs.trigger.top;
+                    })
+                }
+            },
+            onClosePopover(){
+                this.isPop=false
             }
         }
     }
@@ -30,12 +46,11 @@
         display: inline-flex;
         vertical-align: top;
         position: relative;
-        &-content-wrapper{
-            position: absolute;
-            left: 0;
-            bottom: 100%;
-            box-shadow: 0 0 3px rgba(0,0,0,0.5);
-            border: 1px solid red;
-        }
+
+    }
+    &-content-wrapper{
+        position: absolute;
+        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        border: 1px solid red;
     }
 </style>
